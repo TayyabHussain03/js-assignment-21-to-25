26bc3275b7eab7a95b33c2d5db323a7124fad7b0
TayyabHussain <tayyabfaisal106@gmail.com>
2021-05-21 20:55:57 +0500

    question2 added

--- app.js
+++ app.js
@@ -4,3 +4,10 @@
 // var lastName = prompt("Enter Your Last Name Here")
 // var fullName = firstName + " " + lastName
 // var greetings = alert("Welcome!" + " " + fullName)
+
+
+// **********QUESTION 2**********
+
+// var input = prompt("Ask me Your Favourite Mobile Name")
+// document.write("<h3>" + "My Favourite Phone is: " + input + "<h3>")
+// document.write("<h3>" + "Length of string: " + input.length + "<h3>")
